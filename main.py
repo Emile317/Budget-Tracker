966c8fd416e4c4c42a3f5e8fd056a9d277ed493e
Emile Muntinga <emilemuntinga@gmail.com>
2023-02-25 22:59:49 +0100

    added functionality to save data with json

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 from datetime import datetime
+import tkinter as tk
+from tkinter import ttk
+import json
 
-income = 0
-expenses = 0
-income_history = []
-expenses_history = []
+with open("main.json", "r") as json_read:
+    json_data = json.load(json_read)
+
+print(json_data)
 
 def main():
-    summary(income, expenses)
+    summary(json_data["income"], json_data["expenses"])
 
-    functions = [add_income, add_expense, view_history]
+    functions = [add_income, add_expense, view_history, save_and_quit]
     while True:
         choice = input("""Select what you want to do:
 1: Add to your income
 2: Add an expense
 3: View history
-""")
-        if choice.isdigit() == True:
-            if int(choice) > 0 and not int(choice) > len(functions):
-                selection = functions[int(choice)-1]
-                selection()
-                break
-            else:
-                print("Please select one of the options.")
-        else:
-            print("Please input a number.")
+4: Save and quit
+""")    
+        # checks if choice is int and calls chosen function
+        try:
+            int(choice)
+        except ValueError:
+            print("Please input a number")
+            continue
+        functions[int(choice)-1]() if 0 < int(choice) <= len(functions) else print("Please select one of the options")
+        
     
     
 # prints summary of income and expenses
@@ -33,13 +36,12 @@ def summary(income, expenses):
 
 # allows user to add to income
 def add_income():
-    global income, expenses, income_history
     while True:
         added_income = input("Input how much you want to add to your income: ")
         if added_income.isdigit() == True:
             if int(added_income) > 0:   
-                income += int(added_income)
-                time = datetime.now()
+                json_data["income"] += int(added_income)
+                time = datetime.strftime(datetime.now(), "%m/%d/%Y, %H:%M:%S")
                 break
             else:
                 print("Please add an integer above 0.")
@@ -47,18 +49,17 @@ def add_income():
             print("Please input a number.")
     note = input("Add a note (otherwise leave empty): ")
     transaction = [int(added_income), time, note]
-    income_history.append(transaction)
+    json_data["income_history"].append(transaction)
     main()
 
 # allows user to add an expense
 def add_expense():
-    global income, expenses, expenses_history
     while True:
         added_expense = input("Input how much you want to add to your expenses: ")
         if added_expense.isdigit() == True:
             if int(added_expense) > 0:   
-                expenses += int(added_expense)
-                time = datetime.now()
+                json_data["expenses"] += int(added_expense)
+                time = datetime.strftime(datetime.now(), "%m/%d/%Y, %H:%M:%S")
                 break
             else:
                 print("Please add an integer above 0.")
@@ -66,29 +67,50 @@ def add_expense():
             print("Please input a number.")
     note = input("Add a note (otherwise leave empty): ")
     transaction = [int(added_expense), time, note]
-    expenses_history.append(transaction)
+    json_data["expenses_history"].append(transaction)
     main()
 
 def view_history():
     while True:
         choice = input("""Select what you want to do:
-1: View income history
-2: View expenses history
+1: View history
+2: View history of certain time period
 """)
         if choice.isdigit() == True:
             if int(choice) > 0 and not int(choice) > 2:
-                choice2 = income_history if int(choice) == 1 else expenses_history
+                choice2 = json_data["income_history"] if int(choice) == 1 else json_data["expenses_history"]
                 break
             else:
                 print("Please select one of the options.")
         else:
             print("Please input a number.")
 
-    for transaction in choice2:
-        amount = "$" + str(transaction[0])
-        date_time = datetime.strftime(transaction[1], "%m/%d/%Y, %H:%M:%S")
-        note = transaction[2]
-        print("{}\t{}\t{}".format(amount, date_time, note))
+    for transaction in json_data["income_history"]:
+        income_amount = "$" + str(transaction[0])
+        income_date_time = transaction[1]
+        income_note = transaction[2]
     
+    for transaction in json_data["expenses_history"]:
+        expenses_amount = "$" + str(transaction[0])
+        expenses_date_time = transaction[1]
+        expenses_note = transaction[2]
+    
+    print("Income history\t\t\t\tExpenses history")
+    print("{}\t{}\t{}\t\t{}\t{}\t{}".format(income_amount, income_date_time, income_note, expenses_amount, expenses_date_time, expenses_note))
+    main()
+    
+def save_and_quit():
+    with open("main.json", "w") as write_file:
+        json.dump(json_data, write_file, indent=4)
+    quit()
+
+root = tk.Tk()
+root.title("Budget Tracker")
+
+mainframe = ttk.Frame(root)
+mainframe.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
+
+button = ttk.Button(mainframe, text="quit", command=save_and_quit)
+button.grid(column=3, row=3)
 
 main()
